058d46201d07253b1aaa69a2e0ec121f8608cca9
ARUP KUMAR DASH <arupkumard31@gmail.com>
2026-06-26 20:10:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-08f44e8eca9095668"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "GitHubActions-EC2-arup"
