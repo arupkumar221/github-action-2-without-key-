ae192badcb15158d66d96fe31196a53f6bbe1f69
ARUP KUMAR DASH <arupkumard31@gmail.com>
2026-06-26 20:07:38 +0530

    Update AWS region and EC2 instance configuration

--- main.tf
+++ main.tf
@@ -8,12 +8,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0cb473a1f3c06c13d"
-  instance_type = "t3.micro"
+  ami           = "ami-08f44e8eca9095668"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "GitHubActions-EC2-arup"
